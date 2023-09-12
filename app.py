992097cb1d0ec900c7b90d430c5dbff0a92208e0
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-09-12 21:39:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ from BackEndFunctions import CNNClassifier #This is the name of our file/functio
 uploaded_file = st.file_uploader("Please upload image", type=['png','jpeg']) #Add file uploader to take the user's input. limited to .png files only
 if uploaded_file is not None:
         image = Image.open(uploaded_file)                           #Assign the uploded image into a variable dubbed image     
-        st.image(image, caption='جاري تحميل الصورة الرجاء الانتظار', use_column_width=True)     # let user read some word while png is displayed
+        st.image(image, caption='Image uploading, please wait', use_column_width=True)     # let user read some word while png is displayed
         st.write("")                
-        st.write(".... جاري العمل")                                                            # Let user know we are working          
+        st.write(".... Working on it")                                                            # Let user know we are working          
         label = CNNClassifier(image, 'model.h5')                                               #Call the function and then display the test result based on outcome of predcition 
         if label == 0:      
-            st.write("النتيجه سلبية")
+            st.write("Negative")
         else:
-            st.write("النتيجه ايجابيه")
+            st.write("Positive")
             
